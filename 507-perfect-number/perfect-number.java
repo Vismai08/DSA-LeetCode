3ca52e68e125946fd4e46eb10128d08ff737fde0
Vismai S <132152966+Vismai08@users.noreply.github.com>
2026-08-02 18:20:50 +0530

    Time: 1 ms (94.65%) | Memory: 42.4 MB (10.39%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,35 +1,17 @@
-/*class Solution {
-    public boolean checkPerfectNumber(int num) {
-        int y= num;
-        for (int i = 1; i < num; i++) {
-            if ( num % i == 0) {
-                y-=i;
-            }
-
-        }
-        if (y == 0)
-            return true;
-        else
-            return false;
-    }
-}*/
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        // Numbers <= 1 cannot be perfect numbers
-        if (num <= 1) return false;
-
-        int sum = 1; // 1 is always a proper divisor
-
-        // Loop up to sqrt(num)
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                sum += i; // Add the smaller factor
-                if (i * i != num) {
-                    sum += num / i; // Add the matching larger factor
-                }
+        if(num<=1)
+        return false;
+        int y= num-1;
+        for (int i = 2; i*i<= num; i++) {
+            if ( num % i == 0) {
+                y-=i;
+                if(i*i!=num){y=y-(num/i);}
             }
-        }
 
-        return sum == num;
+        }
+        
+            return y==0;
+        
     }
 }
